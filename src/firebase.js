84516b9893ec4af6820df6aaa346f7a70796337d
dawnfire05 <dawnfire05@naver.com>
2024-06-28 03:26:03 +0900

    feat:firebase 연동

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvCaXK0qcZ63whhiI8m2nXqFOzL0abP0w",
+  authDomain: "ntwitter-68dd6.firebaseapp.com",
+  projectId: "ntwitter-68dd6",
+  storageBucket: "ntwitter-68dd6.appspot.com",
+  messagingSenderId: "30853414588",
+  appId: "1:30853414588:web:1c40f55df60ed2bfcf1ab9",
+  measurementId: "G-7RDR8KMP1R"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
