c63260ec6149517fc97f66d2e0405e887d114dec
BuildTools <unconfigured@null.spigotmc.org>
2024-07-03 13:04:49 +0900

    feat:detect and change user state

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,21 @@ import { authService } from "fbase";
 import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(()=> {
-    onAuthStateChanged(authService,(user)=>console.log(user), []);
-  })
+    onAuthStateChanged(authService,(user)=>{
+      if (user){
+        setIsLoggedIn(true);
+      }else{
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })}, []);
   console.log(authService.currentUser);
   return (
     <>
-      <AppRouter isLoggedIn = {isLoggedIn}/>
+      {init ? <AppRouter isLoggedIn = {isLoggedIn}/> : "initializing..."}
       <footer>&copy; {new Date().getFullYear()} NTwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) =>{
         const {
