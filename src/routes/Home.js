1ca617a26f851752035a995a9ec83770514ad31d
BuildTools <dawnfire05@naver.com>
2024-07-04 17:09:41 +0900

    ✨ feat:Creating Tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,39 @@
-const Home = () => <span>Home</span>;
+import { useState } from "react";
+import {db} from "fbase"
+import { addDoc, collection } from "firebase/firestore";
+
+const Home = () => {
+    const [nTweet, setNTweet] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await addDoc(collection(db, "nTweets"), {
+            text: nTweet,
+            createdAt: Date.now()
+        })
+        setNTweet("");
+    };
+
+    const onChange = (event) =>{
+        event.preventDefault();
+        const {
+            target: {value},
+        } = event;
+        setNTweet(value);
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                value = {nTweet}
+                onChange={onChange}
+                type = "text"
+                placeholder="What's on your mind?"
+                maxLength={120}
+            />
+            <input type="submit" value="Nweet"/>
+        </form>
+    )
+};
 
 export default Home;
